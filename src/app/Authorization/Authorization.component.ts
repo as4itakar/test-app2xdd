544201f36ff2c0c3733ccc2a1c08618b591f7ee6
Obukhov Vsevolod <sevaobuxov61@gmail.com>
2023-08-21 23:06:32 +0300

    Add Authorization module

---
+++ src/app/Authorization/Authorization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Authorization',
+  templateUrl: './Authorization.component.html',
+  styleUrls: ['./Authorization.component.scss']
+})
+export class AuthorizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
